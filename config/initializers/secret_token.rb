3ac2dc5a53060a7cf3fbc984bbc1df661ff9936a
Fletcher Fowler <fletch@zambonidev.com>
2013-05-04 16:03:22 -0700

    I: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jamhive::Application.config.secret_token = 'a0b80671d1b0b9b7d9fc7688cdcd3e9ae1d78a32704306a2b4550307a646904d445a8c9d5e5b451a3d9fcc66bbc5fd943a42a8cebe96580ee19882edb9269fca'
